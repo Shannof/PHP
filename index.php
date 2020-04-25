0074e0b5117f886ee574744a6847d5e22fc652c8
01CODE_Shannof <37252777+Shannof@users.noreply.github.com>
2020-04-26 04:39:00 +1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    include'header.php';
+?>
+    
+        <td>
+        <h1>Search</h1>
+        <br>
+            <form action="search.php" method="POST">
+                <input type="text" name="search" placeholder="Search">
+                <button typr="submit" name="submit-search">Search</button>
+            </form>
+        </td>
+<?php
+    include'footer.php';
+?>
